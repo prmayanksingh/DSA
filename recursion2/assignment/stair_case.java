b8f529f1c8269947cbc7acdd097a56a84b336ddf
prmayanksingh <prmayanksingh05@gmail.com>
2024-04-18 16:55:20 +0530

    correcting the name of the function

--- recursion2/assignment/stair_case.java
+++ recursion2/assignment/stair_case.java
@@ -1,13 +1,13 @@
 package recursion2.assignment;
 
 public class stair_case {
-    public static int starecase (int n){
+    public static int staircase (int n){
         if (n < 0) return 0;
         if (n == 0|| n == 1) return 1;
 
-        return starecase(n-1) + starecase(n-2) + starecase(n-3);
+        return staircase(n-1) + staircase(n-2) + staircase(n-3);
     }
     public static void main(String[] args) {
-        System.out.println(starecase(4));
+        System.out.println(staircase(4));
     }
 }
